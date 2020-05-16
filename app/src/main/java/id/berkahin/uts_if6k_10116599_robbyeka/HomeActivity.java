43c5dc6016fb275be8ac784ace49a4d40c92cb59
Eka <obyprof@gmail.ccom>
2020-05-16 16:49:04 +0700

    first

---
+++ app/src/main/java/id/berkahin/uts_if6k_10116599_robbyeka/HomeActivity.java
@@ -0,0 +1,4 @@
+package id.berkahin.uts_if6k_10116599_robbyeka;
+
+public class HomeActivity {
+}
---
+++ app/src/main/java/id/berkahin/uts_if6k_10116599_robbyeka/RegisterActivity.java
@@ -0,0 +1,24 @@
+package id.berkahin.uts_if6k_10116599_robbyeka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView txtRegister;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+
+}
